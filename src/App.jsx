17aa38cfb8cc09a8f9a1b1bee122f726a425877c
Debarup <87480898+debarup24@users.noreply.github.com>
2024-12-02 09:35:12 +0530

    restaurant website

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import PopularDishes from "./components/PopularDishes";
+import Partners from "./components/Partner";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <PopularDishes />
+      <Partners />
     </div>
   );
 }
